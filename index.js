8a4fdac5ad651eef4ae69d87afe65442b1806991
Luciano Mammino <lucianomammino@gmail.com>
2017-01-29 18:45:46 +0000

    Added cloudinary upload and image manipulation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Twitter from 'twitter';
 import { Facebook } from 'fb';
+import cloudinary from 'cloudinary';
 import moment from 'moment';
 import { autoRetrieveAccessToken } from './src/fbUtils';
 import { pipePromises } from './src/pipePromises';
@@ -14,6 +15,7 @@ import { addUrlsScore } from './src/addUrlsScore';
 import { sortByScore } from './src/sortByScore';
 import { takeN } from './src/takeN';
 import { retrieveLinksImage } from './src/retrieveLinksImage';
+import { uploadImagesToCloudinary } from './src/uploadImagesToCloudinary';
 
 const twitterClient = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -28,6 +30,12 @@ const fbApp = autoRetrieveAccessToken(new Facebook({
   appSecret: process.env.FACEBOOK_APP_SECRET,
 }));
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const screenName = process.env.TWITTER_SCREEN_NAME;
 const maxTweets = 200;
 const referenceMoment = moment().subtract('1', 'week').startOf('day');
@@ -36,7 +44,12 @@ const print = data => console.log(JSON.stringify(data, null, 2));
 const prettyPrint = (data) => {
   data.forEach((record, i) => {
     console.log(`---- ${i} ----`);
-    print(record);
+    print({
+      title: record.og_object.title,
+      url: record.id,
+      description: record.og_object.description,
+      image: record.image,
+    });
   });
 };
 
@@ -52,5 +65,6 @@ pipePromises(
   sortByScore,
   takeN(7),
   retrieveLinksImage,
-  print,
+  uploadImagesToCloudinary(cloudinary),
+  prettyPrint,
 );
