f7e31c74fb36156442563c91a5c7cc8be00bd892
Luciano Mammino <luciano@planet9energy.com>
2017-02-10 09:24:05 +0000

    Added test for adImageUrls

--- test/addImageUrls.test.js
+++ test/addImageUrls.test.js
@@ -2,13 +2,17 @@ import test from 'ava';
 import { addImageUrls } from '../src/addImageUrls';
 
 test('It should get an image from the metadata if present or a default image if not', (t) => {
-  t.truthy(addImageUrls);
-  // const testLinks = [
-  //   {},
-  // ];
-  //
-  // const linksWithImages = addImageUrls(testLinks);
-  //
-  // t.is(linksWithImages[0].image, 'ogImage');
-  // t.is(linksWithImages[0].image, 'twitterImageSrc');
+  const testLinks = [
+    { metadata: { ogImage: 'ogImage' } },
+    { metadata: { twitterImageSrc: 'twitterImageSrc' } },
+    { metadata: { images: new Set(['image1']) } },
+    { metadata: { host: 'domain.com' } },
+  ];
+
+  const linksWithImages = addImageUrls(testLinks);
+
+  t.is(linksWithImages[0].image, 'ogImage');
+  t.is(linksWithImages[1].image, 'twitterImageSrc');
+  t.is(linksWithImages[2].image, 'image1');
+  t.regex(linksWithImages[3].image, /domain\.com/);
 });
