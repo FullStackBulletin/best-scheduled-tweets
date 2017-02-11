51f0830bced409e5fc5c83b1b9919bb75db36caf
Luciano Mammino <lucianomammino@gmail.com>
2017-02-11 21:04:01 +0000

    Added support for blacklisting urls (#6)

---
+++ test/removeBlacklistedUrls.test.js
@@ -0,0 +1,10 @@
+import test from 'ava';
+import { removeBlacklistedUrls } from '../src/removeBlacklistedUrls';
+
+test('it should remove blacklisted urls from a list of urls', (t) => {
+  const blacklist = [2, 3, 5];
+  const arr = [1, 2, 3, 4, 5, 6];
+  const expectedResult = [1, 4, 6];
+
+  t.deepEqual(removeBlacklistedUrls(blacklist)(arr), expectedResult);
+});
