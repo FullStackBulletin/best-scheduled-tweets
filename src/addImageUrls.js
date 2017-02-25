0cae6efd4e82aa30063bd1bbbca97db24e42c2a6
Luciano Mammino <lucianomammino@gmail.com>
2017-02-25 13:01:03 +0000

    No pick first image in page (#10)

--- src/addImageUrls.js
+++ src/addImageUrls.js
@@ -1,10 +1,7 @@
 import { coalesce } from 'object-path';
 
 export const addImageUrls = links => links.map((link) => {
-  const defaultImage =
-    (link.metadata.images && link.metadata.images.size) ?
-      (Array.from(link.metadata.images))[0] :
-      `http://placehold.it/350x150/ffffff/0000000&text=${encodeURIComponent(link.metadata.host)}`;
+  const defaultImage = `https://placeholdit.imgix.net/~text?txtsize=60&bg=ffd300&txtclr=0000000%26text%3Dblog&txt=${encodeURIComponent(link.metadata.host)}&w=500&h=240`;
   const image = coalesce(link.metadata, ['ogImage', 'twitterImageSrc'], defaultImage);
 
   return { ...link, image };
