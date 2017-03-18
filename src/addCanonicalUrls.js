471da00eba3e27d71530b741453decbb7e239ba3
Luciano Mammino <lucianomammino@gmail.com>
2017-03-18 10:03:02 +0000

    Convert canonical relative urls into absolute ones

--- src/addCanonicalUrls.js
+++ src/addCanonicalUrls.js
@@ -1,3 +1,4 @@
+import { parse, resolve } from 'url';
 import normalize from 'normalize-url';
 import { coalesce } from 'object-path';
 
@@ -5,6 +6,11 @@ export const addCanonicalUrls = linksData =>
   linksData.map((linkData) => {
     let url = coalesce(linkData, ['metadata.ogUrl', 'id']);
     if (url) {
+      const parts = parse(url);
+      if (!parts.hostname) {
+        // the canonical url is relative, we need the absolute one
+        url = resolve(linkData.id, url);
+      }
       url = normalize(url);
     }
 
--- test/addCanonicalUrls.test.js
+++ test/addCanonicalUrls.test.js
@@ -1,15 +1,17 @@
 import test from 'ava';
 import { addCanonicalUrls } from '../src/addCanonicalUrls';
 
-test('It should select the first found value among metadata.ogUrl, id or undefined', (t) => {
+test('It should select the first found value among metadata.ogUrl, id or undefined and normalize relative urls', (t) => {
   const linksData = [
     { metadata: { ogUrl: 'http://foo.bar/1' } },
     { id: 'http://foo.bar/2' },
     { foo: 'bar' },
+    { id: 'http://foo.bar/2', metadata: { ogUrl: '/relative' } },
   ];
 
   const linksDataWithCanonicalUrl = addCanonicalUrls(linksData);
   t.is(linksDataWithCanonicalUrl[0].url, 'http://foo.bar/1');
   t.is(linksDataWithCanonicalUrl[1].url, 'http://foo.bar/2');
   t.is(linksDataWithCanonicalUrl[2].url, undefined);
+  t.is(linksDataWithCanonicalUrl[3].url, 'http://foo.bar/relative');
 });
